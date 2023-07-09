08c88159a8b7e9af72b4cace06bd28ba53a95685
Flutter Developer <93766485+MuhammadBun@users.noreply.github.com>
2023-07-09 17:52:36 +0300

    add our color
    add the colors from figma to constants.dart file

--- lib/core/util/constants.dart
+++ lib/core/util/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 // ============= STYLE CONSTS ==============
 
@@ -6,8 +7,8 @@ const kScaffoldColor = Color(0xffFDFDFD);
 const kRedColor = Color(0xffA90606);
 
 const kPrimaryColor = Color(0xff2D2B4E);
-const kSecondaryColor = Color(0xffFFD465);
-const kOnSecondaryColor = Color(0xff784E00);
+const kSecondaryColor = Color(0xFFFF1D20);
+const kOnSecondaryColor = Color(0XFFFFFFFF);
 const kDangerColor = Color(0xffF56C61);
 
 // Low Opacity Colors
@@ -16,3 +17,16 @@ const kLightPrimaryColor = Color(0xffE7E5F1);
 const kLightSecondaryColor = Color(0xffFFE6A6);
 const kLightDangerColor = Color(0xffFEE2E7);
 const kOnLightDangerColor = Color(0xff783341);
+
+//Muhammad - Custom Our Project
+const primaryColor = Color(0xFFFF1D20);
+const blackColor = Color(0x00000000);
+const whiteColor = Color(0xFFFFFFFF);
+const linkBorderColor = Color(0xFFE3E3E3);
+const buttomButtonsIconColor = Color(0xFFBDBDBD);
+const profileCardBackground = Color(0xFF5B5B5B);
+const usernameTextColor = Color(0xFFF4F4F4);
+const backgroundAddLinkIconColor = Color(0xFFE8E8E8);
+const priorityLinkChooseIcon = Color(0xFF292D32);
+const editLinkIconColor = Color(0xFF00B912);
+const deleteLinkIconColor = Color(0xFFFF1D20);
